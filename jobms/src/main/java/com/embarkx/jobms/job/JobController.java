c36e2ce92223da4a171193a85241aa9d7399921a
parshu <parshuram.m@accops.com>
2025-03-20 20:10:46 +0530

    Added Code for Job Application.

---
+++ jobms/src/main/java/com/embarkx/jobms/job/JobController.java
@@ -0,0 +1,56 @@
+package com.embarkx.jobms.job;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/jobs")
+public class JobController {
+    private JobService jobService;
+
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    @GetMapping
+    public  ResponseEntity<List<Job>> findAll() {
+        return new ResponseEntity<>(jobService.findAll(),HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createJob(@RequestBody Job job) {
+        jobService.createJob(job);
+        return new ResponseEntity<>("Job added successfully.",HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Job> getJobById(@PathVariable Long id) {
+
+        Job job = jobService.getJobById(id);
+        if (job != null)
+            return new ResponseEntity<>(job, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteJobById(@PathVariable Long id) {
+        boolean deleted = jobService.deleteJobById(id);
+        if (deleted)
+            return new ResponseEntity<>("Job Deleted successfully", HttpStatus.OK);
+        return new ResponseEntity<>("Not able to find the job with Given Id:" + id, HttpStatus.NOT_FOUND);
+    }
+
+    //@PutMapping("jobs/{id}")
+    @RequestMapping(value="/{id}", method=RequestMethod.PUT)
+    public  ResponseEntity<String> updateJob(@PathVariable Long id,@RequestBody Job updatedJob)
+    {
+        boolean updated=jobService.updateJob(id,updatedJob);
+        if(updated)
+            return new ResponseEntity<>("Job updated Successfuly",HttpStatus.OK);
+        else
+            return new ResponseEntity<>("Job updation Failed",HttpStatus.NOT_FOUND);
+    }
+}
