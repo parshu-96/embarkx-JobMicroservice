d92a980d9a7e02c349fe26c5d7c88254e0576256
parshu <parshuram.m@accops.com>
2025-03-24 20:00:25 +0530

    Adding Rest template to Job Microservice

--- jobms/src/main/java/com/embarkx/jobms/job/impl/JobServiceImpl.java
+++ jobms/src/main/java/com/embarkx/jobms/job/impl/JobServiceImpl.java
@@ -4,7 +4,9 @@ package com.embarkx.jobms.job.impl;
 import com.embarkx.jobms.job.Job;
 import com.embarkx.jobms.job.JobRepository;
 import com.embarkx.jobms.job.JobService;
+import com.embarkx.jobms.job.external.Company;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,6 +22,10 @@ public class JobServiceImpl implements JobService {
 
     @Override
     public List<Job> findAll() {
+        RestTemplate restTemplate = new RestTemplate();
+        Company company= restTemplate.getForObject("http://localhost:8081/companies/1", Company.class);
+        System.out.println("Company :" +company.getName());
+        System.out.println("Company ID:" +company.getId());
         return jobRepository.findAll();
     }
 
